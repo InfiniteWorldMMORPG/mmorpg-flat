adb1fab8b3664e58a5589d4e3385055ca7d169f9
Igor Kostyakov <i.a.kostyakov@gmail.com>
2024-06-02 23:22:20 +0300

    Add DB Scheme

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# Some Unnamed MMORPG
+
+![Database schema](assets/DatabaseScheme.v1.svg)
+
+https://www.dbdiagram.io/d/MMORPG-665ccf6cb65d9338794d0f9a
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -12,8 +12,8 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '#': fileURLToPath(new URL('./src', import.meta.url)),
       '#modules': fileURLToPath(new URL('./modules', import.meta.url)),
+      '#': fileURLToPath(new URL('./src', import.meta.url)),
     }
   }
 });
